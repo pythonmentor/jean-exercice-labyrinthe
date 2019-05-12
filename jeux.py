277c83a5d11cf0effe3cb63104e2d37270f779d8
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-05-13 00:12:36 +0200

    Mise à jour de l'affichage de la lecture ligne par ligne du labyrinthe.

--- jeux.py
+++ jeux.py
@@ -1,12 +1,18 @@
-import pickle
 
-# 
-i = 0
 with open("labyrinthe.txt", "r") as mon_fichier:
     mon_labyrinthe = mon_fichier.readlines()
 
     print("1. Lecture du fichier labyrinthe.txt ligne par ligne:\n")
 
     for i, elt in enumerate(mon_labyrinthe):
-        print("À la ligne {} se trouve {}".format(i,elt))
+        print(i, elt)
+
+
+    print("2. Ajout de la position (numéro de ligne, numéro de colonne) de chaque chemin (.):\n")
+
+   
+
+   
+
+        
 
