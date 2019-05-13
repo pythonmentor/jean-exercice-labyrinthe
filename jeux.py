b4bf4dbbf9cc324910ee5cae05c5d17108889e40
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-05-13 02:37:58 +0200

    Ajout des fonctionnalités largeur et hauteur du labyrinthe.

--- jeux.py
+++ jeux.py
@@ -8,7 +8,31 @@ with open("labyrinthe.txt", "r") as mon_fichier:
         print(i, elt)
 
 
-    print("2. Ajout de la position (numéro de ligne, numéro de colonne) de chaque chemin (.):\n")
+    with open("labyrinthe.txt", "r") as fichier:
+        mon_fichier_2 = fichier.readline().strip()
+
+        print("4. Calcul de la largeur et la hauteur du labyrinthe:")
+
+        largeur = len(mon_fichier_2)
+        print("La largeur du labyrinthe vaut:", largeur)
+
+       
+        mon_fichier_3 = fichier.readlines()
+
+        aire_du_labyrinthe = len("".join(mon_fichier_3))
+
+
+        hauteur = aire_du_labyrinthe / largeur
+        print("La hauteur vaut: ", hauteur)
+
+
+         
+        
+
+
+        
+        
+        
 
    
 
