0d45a9b7580f20f723aca30e94a968c913f4ce85
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-05-20 04:20:28 +0200

    Affichage des valeurs de la largeur et la hauteur du labyrinthe.

--- jeux.py
+++ jeux.py
@@ -19,13 +19,11 @@ def main():
 
     with open("labyrinthe.txt", "r") as mon_fichier:
         mon_labyrinthe = mon_fichier.readlines()
-        hauteur = len(mon_labyrinthe)
-        largeur = len(mon_labyrinthe[0].strip())
-
+        
         print("Lecture du fichier labyrinthe.txt ligne par ligne:\n")
         print(mon_labyrinthe)
 
-        print("Determination de la position des chemins et murs: ")
+        print("Determination de la position des chemins et des murs: ")
         for i, ligne in enumerate(mon_labyrinthe):
             for j, caractere in enumerate(ligne.strip()):
                 if caractere == '.':
@@ -38,6 +36,12 @@ def main():
             print("Les chemins ", chemins,)
             print("Les murs", murs)
 
+        print("Détermination de la largeur et la hauteur du labyrinthe:")
+        hauteur = len(mon_labyrinthe)
+        largeur = len(mon_labyrinthe[0].strip())
+
+        print("La largeur vaut: ", largeur)
+        print("La hauteur vaut: ", hauteur)
 
         
 main()
